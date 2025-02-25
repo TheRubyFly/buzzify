0ed64c4e36290ae7763a7b757c3657d558b7d1d1
Fluffye <camille.pellat@gmail.com>
2025-02-25 17:07:56 +0100

    environement

---
+++ Frontend/src/pages/Environement.jsx
@@ -0,0 +1,13 @@
+const Environnement = () => {
+    const environnement = import.meta.env.VITE_ENVIRONNEMENT || "local";
+    const apiUrl = import.meta.env.VITE_API_URL || "localhost:8000";
+    return (
+      <div>
+        <div>
+          Environnement de déploiement : {environnement}, url d'api : {apiUrl}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Environnement;
