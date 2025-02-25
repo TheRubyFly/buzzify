b34eca855d14ca7a295f22797ccc311baec4be58
Fluffye <camille.pellat@gmail.com>
2025-02-25 14:10:13 +0100

    add page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,8 +4,10 @@ function Home(){
     return(
         <div className="bruh">
         <h1>Buzzify</h1>
-        <div><button className="buzzer">Créer une salle</button></div>
-        <div><button >Rejoindre une salle</button></div>
+        <div>
+            <button className="buzzer">Créer une salle</button>
+            <button >Rejoindre une salle</button>
+        </div>
         </div>
     );
 
