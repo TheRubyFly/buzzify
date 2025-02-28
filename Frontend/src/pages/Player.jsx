d4077856236f9f7fdd869b08d9a14455ad9730b7
Gaspard Freydier <gaspard.freydier@gmail.com>
2025-02-28 12:42:40 +0100

    bouton home

--- Frontend/src/pages/Player.jsx
+++ Frontend/src/pages/Player.jsx
@@ -1,10 +1,13 @@
 
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
+import { useNavigate } from "react-router-dom";
+
 
 function Player() {
     const [buzzed, setBuzzed] = useState(null);
     const socket = io("http://localhost:5000");
+    const navigate = useNavigate();
 
     useEffect(() => {
         socket.on("buzzed", (data) => {
@@ -28,10 +31,25 @@ function Player() {
     };
 
     return (
-        <div className="bruh">
+        <div>
+            <div>
+                 <button 
+                 className="home-button" 
+                 onClick={() => navigate("/")}>
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" width="24" height="24"/>
+                </button>
+            </div>
             <h1>Buzzify</h1>
-            <div>{buzzed ? <h2>{buzzed} a buzzé !</h2> : <button className="buzzer" onClick={handleBuzz}>Buzz</button>}</div>
-            <div><button onClick={handleReset}>Réinitialiser</button></div>
+            <div>{buzzed ? <h2>{buzzed} a buzzé !</h2> : 
+                <button 
+                    className="buzzer" 
+                    onClick={handleBuzz}>
+                    <img src="https://www.espace-orthophonie.fr/1244519-large_default/buzzer.jpg" width="168" height="168"/>
+                </button>}
+            </div>
+            <div>
+                <button onClick={handleReset}>Réinitialiser</button>
+            </div>
         </div>
     );
 }
