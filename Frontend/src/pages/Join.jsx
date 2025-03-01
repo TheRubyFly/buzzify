ee2dbdcc9235d5292ff192e106ca6f95172adb33
Gaspard Freydier <gaspard.freydier@gmail.com>
2025-03-01 16:31:22 +0100

    bugfix

--- Frontend/src/pages/Join.jsx
+++ Frontend/src/pages/Join.jsx
@@ -1,56 +1,44 @@
-
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function MyForm() {
-    // États pour stocker les valeurs des inputs
     const [code, setCode] = useState("");
     const [pseudo, setPseudo] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
 
     function handleSubmit(e) {
-        e.preventDefault(); // Empêche le rechargement de la page
+        e.preventDefault();
 
-        // Vérification : les champs ne doivent pas être vides
         if (!code.trim() || !pseudo.trim()) {
             setError("Veuillez remplir tous les champs !");
-            return; // Arrête l'exécution ici si un champ est vide
+            return;
         }
 
-        // Si tout est bon, on peut envoyer les données
+        setError("");
         console.log("Rejoindre la salle avec:", { code, pseudo });
-        setError(""); // Efface le message d'erreur
+        navigate("/Player");
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                 <button 
-                 className="home-button" 
-                 onClick={() => navigate("/")}>
-                    <img src="https://banner2.cleanpng.com/20180411/ike/avfjoey57.webp" width="24" height="24"/>
+                <button 
+                    className="home-button" 
+                    onClick={() => navigate("/")}>
+                    <img src="https://banner2.cleanpng.com/20180411/ike/avfjoey57.webp" width="72" height="72"/>
                 </button>
             </div>
             <div className="remplir">
                 <label>
                     Code de la salle{" "}
-                    <input
-                        type="text"
-                        value={code}
-                        onChange={(e) => setCode(e.target.value)}
-                    />
+                    <input type="text" value={code} onChange={(e) => setCode(e.target.value)} />
                 </label>
             </div>
             <div className="remplir">
                 <label>
                     Pseudo{" "}
-                    <input
-                        type="text"
-                        value={pseudo}
-                        onChange={(e) => setPseudo(e.target.value)}
-                    />
+                    <input type="text" value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
                 </label>
             </div>
             {error && <p style={{ color: "red" }}>{error}</p>}
